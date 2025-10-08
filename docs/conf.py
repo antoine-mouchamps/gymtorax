07b269fb51a98be83951c1662e53e8a99c74e87c
Antoine <antoine.mouchamps@gmail.com>
2025-10-08 16:18:37 +0200

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -117,6 +117,7 @@ copybutton_remove_prompts = True
 
 html_theme = "sphinx_rtd_theme"
 html_logo = "Images/logo.png"
+html_favicon = "Images/favicon.ico"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 
